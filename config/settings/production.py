e540ef4ebe6dbb3405cbdb73b0fb368d9dc007fd
STQT <productgsm@gmail.com>
2024-07-09 09:37:00 +0000

    server changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -77,7 +77,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
 }
 
